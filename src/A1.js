55f35842bd5f9e92b737f05e0a1361edfba11b2c
apsingh001 <64854893+apsingh001@users.noreply.github.com>
2022-03-09 13:07:46 +0530

    Add edit button

--- src/A1.js
+++ src/A1.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import A2 from "./A2";
 function AA(props){
     const [ob,setOb]=React.useState(props.obj);
     const [col,setCol]=React.useState("black");
+    const [names,setNames]=React.useState(ob.name);
+    const [phones,setPhones]=React.useState(ob.phone);
+    const [emails,setEmails]=React.useState(ob.email);
+    const [lats,setLats]=React.useState(ob.address.geo.lat);
+    const [longs,setLongs]=React.useState(ob.address.geo.lng);
+    const [dcc,setDcc]=React.useState(0);
+    const [dccc,setDccc]=React.useState(0);
     let ap="aa";
     if(ob.id%2===0){
         ap="white";
@@ -16,7 +22,7 @@ function AA(props){
     if(dc===0){
         return <div></div>;
     }
-    else if(dc===1){
+    if(dc===1){
         return <div id="undo">
             <button id="undo1" type="button" onClick={function(){
                 setDc(2);
@@ -26,16 +32,50 @@ function AA(props){
             }}>Confirm Delete</button>
         </div>;
     }
+    if(dcc===1){
+        return <div class="aps">
+            <div><input  type="text" id="i1" name="a111" onChange={event=>setNames(event.target.value)}></input></div>
+            <div><input  type="text" id="i2" onChange={event=>setPhones(event.target.value)}></input></div>
+            <div><input  type="text" id="i3" onChange={event=>setEmails(event.target.value)}></input></div>
+            <div><input  type="text" id="i4" onChange={event=>setLats(event.target.value)}></input></div>
+            <div><input  type="text" id="i5" onChange={event=>setLongs(event.target.value)}></input></div>
+            <div>
+            <span><button type="button" onClick={function(){setDccc(1);setDcc(0);}}>Done</button></span> 
+
+            </div>
+            </div>
+    }
+    console.log(names);
+    if(dccc===1){
+        // setDccc(0);
+        return (
+            
+            <div class="aps" style={style}>
+                <div><input type="checkbox" onClick={function(){if(col==="black"){setCol("red")}else{setCol("black")}}}></input>{names}</div>
+                <div>{phones}</div>
+                <div>{emails}</div>
+                <div>{lats}</div>
+                <div>{longs}</div>
+                <div>
+                <span><button type="button" onClick={function(){setDcc(1)}}>Edit</button></span>&nbsp;
+                <span><button type="button" onClick={function(){ setDc(1)
+                }}>Delete</button></span> 
+                </div>
+    
+    
+            </div>
+        )
+    }
     return (
         
-        <div id="aps" style={style}>
+        <div class="aps" style={style}>
             <div><input type="checkbox" onClick={function(){if(col==="black"){setCol("red")}else{setCol("black")}}}></input>{ob.name}</div>
             <div>{ob.phone}</div>
             <div>{ob.email}</div>
             <div>{ob.address.geo.lat}</div>
             <div>{ob.address.geo.lng}</div>
             <div>
-            <span><button type="button">Edit</button></span>&nbsp;
+            <span><button type="button" onClick={function(){setDcc(1)}}>Edit</button></span>&nbsp;
             <span><button type="button" onClick={function(){ setDc(1)
             }}>Delete</button></span> 
             </div>
